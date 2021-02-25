4882bf85e0c4deb7c8dbf16e37081cdf77d2bdc8
Caro <bellacarolita@gmail.com>
2021-02-25 14:23:20 -0800

    1 2 3

--- Ejercicios/Calificaciones/ex3.py
+++ Ejercicios/Calificaciones/ex3.py
@@ -1,6 +1,18 @@
 
 # 3. Determinar la edad de una persona conociendo el año actual y el año de nacimiento.
 
+print("Ingresa año actual")
+year_hoy = int(input())
+
+print("Ingresa el ano de tu nacimiento")
+year_nacimiento = int(input())
+
+edad_anos = year_hoy -  year_nacimiento
+print(edad_anos)
+
+
+
+
 
 
 
