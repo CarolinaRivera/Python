ed9354d662585a79051cbe4d7468861679039622
Caro <bellacarolita@gmail.com>
2021-02-25 14:14:16 -0800

    Ejercicios 1 2

--- Ejercicios/Calificaciones/ex2.py
+++ Ejercicios/Calificaciones/ex2.py
@@ -4,12 +4,13 @@
 #print("Calcula el area de un circulo")
 
 pi = 3.1416
+print("Ingresa un valor para radio: ")
 radius = int(input())
 
 
 circulo_area = pow(radius, 2) * 3.1416
 circulo_perimetro = (2* pi *radius)
 
-print(circulo_area)
-print(circulo_perimetro)
+print(f"el area de un circulo es {circulo_area}")
+print(f"El perimetro de un circulo es {circulo_perimetro}")
 
