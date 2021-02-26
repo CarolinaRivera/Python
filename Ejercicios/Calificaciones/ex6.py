cb99f77c1c9fbc2922564a313b531ae7a4928e07
Caro <bellacarolita@gmail.com>
2021-02-26 15:45:23 -0800

    ex6

--- Ejercicios/Calificaciones/ex6.py
+++ Ejercicios/Calificaciones/ex6.py
@@ -5,20 +5,26 @@ import math
 
 print("ingresa los valores para la creacion de la formula general")
 
-a = float(input())
-b = float(input())
-c = float(input())
+a = float(input("a: "))
+b = float(input("b: "))
+c = float(input("c: "))
 
 # pow(number1, 0.5)
 x1 = (-b + pow(( b ** b )-(4 * a * c), 0.5))  / (2 * a)
-x2 = (-b + math.sqrt(( b ** b )-(4 * a * c))) / (2 * a)
-x3 = (-b - math.sqrt(( b ** b )-(4 * a * c))) / (2 * a)
+x2 = (-b - pow(( b ** b )-(4 * a * c), 0.5))  / (2 * a)
+
+x3 = (-b + math.sqrt(( b ** b )-(4 * a * c))) / (2 * a)
+x4 = (-b - math.sqrt(( b ** b )-(4 * a * c))) / (2 * a)
 
 #x2 = (b + (( b ** b )-(4 * a * c))) / 2 * a
 
-print(x1)
-print(x2)
-print(x3)
+print(f" X! = ", x1)
+print(f" X1 = ", round(x1, 2))
+print(f" X2 = ", round(x2, 2))
+print(f" X3 = ", round(x3, 2))
+print(f" X4 = ", round(x4, 2))
+
+
 
 
 
