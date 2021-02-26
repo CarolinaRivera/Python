2b73e53462a3c34d8c6e5a9ae438406a724202e7
Caro <bellacarolita@gmail.com>
2021-02-26 15:12:02 -0800

    ex 6 enproceso

--- Ejercicios/Calificaciones/ex6.py
+++ Ejercicios/Calificaciones/ex6.py
@@ -1,5 +1,23 @@
 
 # 6. Obtener valores para: a, b y c. Calcular la fórmula general.
 
+import math
+
+print("ingresa los valores para la creacion de la formula general")
+
+a = float(input())
+b = float(input())
+c = float(input())
+
+# pow(number1, 0.5)
+#x1 = (-b + pow(( b ** b )-(4 * a * c)), 0.5) / (2 * a)
+x1 = (-b +                    (( b ** b )-(4 * a * c))) / (2 * a)
+
+#x2 = (b + (( b ** b )-(4 * a * c))) / 2 * a
+
+print(x1)
+#print(x2)
+
+
 
 
